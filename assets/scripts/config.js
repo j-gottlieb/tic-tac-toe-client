09d3ef89affa7455e9b5431b8217cbc5351ee93e
j-gottlieb <gessejottlieb@gmail.com>
2018-09-26 09:07:43 -0400

    Curl-Scripts
    sign-up, sign-in, sign-out, change-password scripts all successfully
    communicated with the api

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
+  production: 'https://aqueous-atoll-85096.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
---
+++ curl-scripts/sign-out.sh
@@ -0,0 +1,7 @@
+curl "https://aqueous-atoll-85096.herokuapp.com/sign-out" \
+  --include \
+  --header "Authorization: Token token=${TOKEN}" \
+  --request DELETE
+
+
+echo
