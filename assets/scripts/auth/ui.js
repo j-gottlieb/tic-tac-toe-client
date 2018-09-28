8afb5ca42a4da5dc4e3061ef1b4406284647a3c0
j-gottlieb <gessejottlieb@gmail.com>
2018-09-28 11:48:43 -0400

    Fiddled with css
    Could not get minute movements to stop when fading out messages.

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -1,52 +1,56 @@
 const store = require('../store.js')
 
 const signUpSuccess = function (response) {
-  $('#display-message').html('')
+  $('#display-message').html('').hide()
   $('#sign-up-form').trigger('reset')
-  $('#display-message').append(`<p>Success! </p>`)
+  $('#display-message').text(`Success!`).fadeToggle().delay(1000).fadeToggle()
   $('#display-message').css('color', 'green')
   $('#sign-up-btn, #sign-up-form').addClass('hidden')
 }
 
 const signUpFailure = function () {
-  $('#display-message').html('Something went wrong, please try again')
+  $('#display-message').html('').hide()
+  $('#display-message').html('Something went wrong, please try again').fadeToggle().delay(1000).fadeToggle()
   $('#display-message').css('color', 'red')
   $('#sign-up-form').trigger('reset')
 }
 
 const signInSuccess = function (response) {
-  $('#display-message').html('')
+  $('#display-message').html('').hide()
   $('#sign-in-form').trigger('reset')
   $('#display-message').css('color', 'green')
   $('#sign-out-form,.game-buttons,#change-password-btn,#new-game').removeClass('hidden')
   $('#sign-in-btn,#sign-up-btn,#sign-in-form').addClass('hidden')
   store.user = response.user
-  $('#display-message').append(`<p>Welcome, ${store.user.email} </p>`)
+  $('#display-message').text(`Welcome, ${store.user.email}`).fadeToggle().delay(1000).fadeToggle()
 }
 
 const signInFailure = function () {
-  $('#display-message').html('Something went wrong, please try again')
+  $('#display-message').html('').hide()
+  $('#display-message').text('Something went wrong, please try again').fadeToggle().delay(1000).fadeToggle()
   $('#display-message').css('color', 'red')
   $('#sign-up-form').trigger('reset')
 }
 
 const changePasswordSuccess = function (response) {
-  $('#display-message').html('')
+  $('#display-message').html('').hide()
   $('#change-password-form').trigger('reset')
-  $('#display-message').append(`<p>Successfully changed password!</p>`)
+  $('#display-message').text(`Successfully changed password!`).fadeToggle().delay(1000).fadeToggle()
   $('#display-message').css('color', 'green')
 }
 
 const changePasswordFailure = function () {
-  $('#display-message').html('Something went wrong, please try again')
+  $('#display-message').html('').hide()
+  $('#display-message').text('Something went wrong, please try again').fadeToggle().delay(1000).fadeToggle()
   $('#display-message').css('color', 'red')
   $('#sign-up-form').trigger('reset')
 }
 
 const signOutSuccess = function () {
-  // console.log('youre singing out')
+  $('#display-message').html('').hide()
   $('#sign-up-form, #sign-in-form, #change-password-form').trigger('reset')
-  $('#display-message').html('You have successfully signed out')
+  $('#display-message').removeClass('hidden')
+  $('#display-message').text(`You have successfully signed out`).fadeToggle().delay(1000).fadeToggle()
   $('#display-message').css('color', 'green')
   // console.log('where are my buttons?')
   $('#sign-out-form, .game-board, #change-password-btn, #change-password-form, #new-game, .game-buttons').addClass('hidden')
